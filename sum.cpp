4e864d4e11a3709fc5b3918cb128e54de1190af8
rohankumar37 <rohankum.37@gmail.com>
2021-06-01 13:38:34 +0530

    adding sum.exe

--- sum.cpp
+++ sum.cpp
@@ -10,6 +10,6 @@ int main()
     int a, b;
     cout << "enter two numbers:\n";
     cin >> a >> b;
-    cout << "The sum is " << sum(a, b);
+    cout << "The sum of two numbers is " << sum(a, b);
     return 0;
 }
