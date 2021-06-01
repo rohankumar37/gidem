622f5de9ce1f6bd7a92dd50440872117f8e734a3
rohankumar37 <rohankum.37@gmail.com>
2021-06-01 13:31:13 +0530

    initial commit

---
+++ sum.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int sum(int x, int y)
+{
+    int sumOfNumbers = x + y;
+    return sumOfNumbers;
+}
+int main()
+{
+    int a, b;
+    cout << "enter two numbers:\n";
+    cin >> a >> b;
+    cout << "The sum is " << sum(a, b);
+    return 0;
+}
